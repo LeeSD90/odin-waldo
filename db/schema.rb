8681cea15d1e353974099e63f8785441788b90dd
LeeSD90 <lee.doyle90@gmail.com>
2018-09-12 12:36:48 +0200

    migrate and add seed data

--- db/schema.rb
+++ db/schema.rb
@@ -10,16 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_09_12_102142) do
+ActiveRecord::Schema.define(version: 2018_09_12_103012) do
 
   create_table "puzzles", force: :cascade do |t|
     t.string "title"
+    t.string "image"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "image_file_name"
-    t.string "image_content_type"
-    t.bigint "image_file_size"
-    t.datetime "image_updated_at"
   end
 
 end
