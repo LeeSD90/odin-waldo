d1bd083304d8d6177e8e43e7d7a1b40edca7b201
LeeSD90 <lee.doyle90@gmail.com>
2018-09-19 13:02:00 +0200

    update seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,11 +10,18 @@
 p1 = Puzzle.create(title: "Ali Baba And The Forty Thieves", image: "waldo1.jpg")
 p1.characters.create(:name => "waldo", :x => 1020, :y => 533)
 p1.characters.create(:name => "odlaw", :x => 1303, :y => 478)
+p1.characters.create(:name => "wenda", :x => 1212, :y => 167)
+p1.characters.create(:name => "wizard_whitebeard", :x => 143, :y => 567)
 
 
-#p2 = Puzzle.create(title: "Horseplay In Troy", image: "waldo2.jpg")
+p2 = Puzzle.create(title: "Horseplay In Troy", image: "waldo2.jpg")
+p2.characters.create(:name => "waldo", :x => 220, :y => 725)
+p2.characters.create(:name => "odlaw", :x => 1237, :y => 702)
+p2.characters.create(:name => "wenda", :x => 1078, :y => 645)
+p2.characters.create(:name => "wizard_whitebeard", :x => 398, :y => 50)
 
-
-#Puzzle.create(title: "The Deep-Sea Divers", image: "waldo3.jpg")
-
-#Puzzle.create(title: "test", image: "waldo1.jpg")
+p3 = Puzzle.create(title: "The Deep-Sea Divers", image: "waldo3.jpg")
+p3.characters.create(:name => "waldo", :x => 928, :y => 146)
+p3.characters.create(:name => "odlaw", :x => 421, :y => 173)
+p3.characters.create(:name => "wenda", :x => 724, :y => 215)
+p3.characters.create(:name => "wizard_whitebeard", :x => 1099, :y => 116)
