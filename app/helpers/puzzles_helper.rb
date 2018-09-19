8c8a491199eaec49f5d7f54a1bcf921413e69133
LeeSD90 <lee.doyle90@gmail.com>
2018-09-19 12:22:36 +0200

    add recording of

--- app/helpers/puzzles_helper.rb
+++ app/helpers/puzzles_helper.rb
@@ -1,2 +1,13 @@
 module PuzzlesHelper
+  def setup
+    session[:remaining] = @puzzle.characters.map(&:name)
+  end
+
+  def update_remaining(char)
+    session[:remaining].delete(char)
+  end
+
+  def complete?
+    return session[:remaining].empty?
+  end
 end
