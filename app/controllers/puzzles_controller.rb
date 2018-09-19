af4c05261f48edbdd0671f35086c706abd67cbfb
LeeSD90 <lee.doyle90@gmail.com>
2018-09-19 12:30:03 +0200

    add record of game progress to session cookie

--- app/controllers/puzzles_controller.rb
+++ app/controllers/puzzles_controller.rb
@@ -5,12 +5,18 @@ class PuzzlesController < ApplicationController
 
   def show
     @puzzle = Puzzle.find(params[:id])
+    setup
   end
 
   def check
     logger.debug params
     @puzzle = Puzzle.find(params[:id])
+
     correct = @puzzle.check(params[:character], params[:x], params[:y])
+
+    if correct
+      update_remaining(params[:character])
+    end
     render json:  correct
   end
 
