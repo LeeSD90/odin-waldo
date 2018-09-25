98a415d9ee632758349d4db14326022922ef5d95
LeeSD90 <lee.doyle90@gmail.com>
2018-09-25 10:27:40 +0200

    Fix migration error

--- db/migrate/20180925082021_create_scores.rb
+++ db/migrate/20180925082021_create_scores.rb
@@ -2,7 +2,7 @@ class CreateScores < ActiveRecord::Migration[5.2]
   def change
     create_table :scores do |t|
       t.string :name
-      t.time :float
+      t.float :time
       t.references  :puzzle
 
       t.timestamps
