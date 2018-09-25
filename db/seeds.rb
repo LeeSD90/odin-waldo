1b22e2e77175a86864cb735afbeba79d79f8e8b7
LeeSD90 <lee.doyle90@gmail.com>
2018-09-25 10:31:32 +0200

    update seed names

--- db/seeds.rb
+++ db/seeds.rb
@@ -13,8 +13,8 @@ p1.characters.create(:name => "odlaw", :x => 1303, :y => 478)
 p1.characters.create(:name => "wenda", :x => 1212, :y => 167)
 p1.characters.create(:name => "wizard_whitebeard", :x => 143, :y => 567)
 p1.scores.create(:name => "Bob", :time => 54.5)
-p1.scores.create(:name => "Bob", :time => 10.234)
-p1.scores.create(:name => "Bob", :time => 723.123)
+p1.scores.create(:name => "Bill", :time => 10.234)
+p1.scores.create(:name => "Barney", :time => 723.123)
 
 
 p2 = Puzzle.create(title: "Horseplay In Troy", image: "waldo2.jpg")
