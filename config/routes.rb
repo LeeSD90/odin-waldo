43fa3304f21483bd8c2078c913fd036b777410c5
LeeSD90 <lee.doyle90@gmail.com>
2018-09-26 11:19:19 +0200

    add controller routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   resources :puzzles, only: [:index, :show]
 
   get '/check', to: 'puzzles#check'
-  
+  post '/add_score', to: 'puzzles#add_score'
   root 'puzzles#index'
 end
